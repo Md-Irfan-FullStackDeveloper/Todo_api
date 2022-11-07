6c5d19e6818016eb88afb42f1d665e33c009f4d2
Md Irfan <irfanjunaid78081@gmail.com>
2022-11-07 20:03:02 +0530

    updated the package file

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const cors = require("cors");
 const { userRouter } = require("./Routes/user.route");
 const { todoRouter } = require("./Routes/todo.route");
 
-const app = express();
 const PORT = process.env.PORT || 5000;
+const app = express();
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
 app.get("/api", (req, res) => {
   res.send("Welcome to api homepage");
